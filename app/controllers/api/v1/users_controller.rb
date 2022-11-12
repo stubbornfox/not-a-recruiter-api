a0f4c87dd27c3018c63fc83952237bc5a7d84b9a
Ha Le <nhiha001@gmail.com>
2022-11-13 02:11:35 +0700

    [f] Create user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[ show update destroy ]
+  before_action :authorize_request, except: :create
 
   # GET /users
   def index
@@ -16,9 +17,8 @@ class Api::V1::UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user.email, status: :created, location: api_v1_user_path(@user)
     else
       render json: @user.errors, status: :unprocessable_entity
     end
