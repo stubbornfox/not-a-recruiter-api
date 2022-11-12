f3ebf7f8fa1f85d50154a28152cc4947404e6dde
Ha Le <nhiha001@gmail.com>
2022-11-13 00:45:05 +0700

    [f] Remove byebug

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::AuthenticationController < ApplicationController
   # POST /auth/login
   def login
     @user = User.find_by_email(params[:email])
-    byebug
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Time.now + 24.hours.to_i
