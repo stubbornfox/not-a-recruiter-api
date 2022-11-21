2c021be9733103d66db9a6e0eaed36d15202bf49
Ha Le <nhiha001@gmail.com>
2022-11-22 04:22:50 +0700

    [f] specific on cors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
-  config.allowed_cors_origins = "app.ahriha.me"
+  config.allowed_cors_origins = ["https://app.ahriha.me"]
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
