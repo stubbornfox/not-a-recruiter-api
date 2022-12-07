3c7c24ecbd6ad3ec485bb73a2ab9a745f5f207ff
Ha Le <nhiha001@gmail.com>
2022-12-07 22:31:22 +0700

    [f] handle preview job by rails app

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+  def index
+    custom_domain_url = request.original_url
+    if @job_board = JobBoard.find_by_custom_domain_url(custom_domain_url)
+      @organization = @job_board.organization
+      render :job_board
+      return
+    else
+      @organization = Organization.find_by(slug: params[:id])
+      @job_board = @organization.job_board
+      render :job_board
+      return
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -26,8 +26,10 @@ Rails.application.routes.draw do
       end
     end
   end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get "/:id" => "home#index"
+  root "home#index"
 end
