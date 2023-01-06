4ce11dad3642d7fd93bb561899b54649f53c027e
Ha Le <nhiha001@gmail.com>
2023-01-06 16:27:37 +0700

    [f] remove hosts in env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.hosts << /.*\.qrhire\.net/
+  # config.hosts << /.*\.qrhire\.net/
   config.action_mailer.delivery_method = :smtp
   host = 'qrhire.net' #replace with your own url
   config.action_mailer.default_url_options = { host: host }
