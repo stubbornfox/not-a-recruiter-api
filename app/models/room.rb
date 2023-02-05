1d1ab134fdf4835c64f9f55ef00e16ce57f674de
Ha Le <nhiha001@gmail.com>
2023-02-05 12:04:33 +0700

    [f] check for exist room

--- app/models/room.rb
+++ app/models/room.rb
@@ -15,19 +15,26 @@
 class Room < ApplicationRecord
   has_many :participants, dependent: :destroy
   has_many :users, through: :participants
-  has_many :messages
+  has_many :messages, dependent: :destroy
   belongs_to :organization
 
   scope :recent, -> { joins(:messages).order(updated_at: :desc) }
 
   def self.create_room(user_ids, organization)
-    room = Room.new(organization: organization)
-    user_ids.each do |user_id|
-      room.participants.new(user_id: user_id)
+    p = Participant.joins(:room).group('room_id')
+        .having("array_agg(participants.user_id order by participants.user_id)::int[] = array#{user_ids.sort}")
+        .select('room_id').where('rooms.organization_id': organization.id)
+    if p.present?
+      p.first.room
+    else
+      room = Room.new(organization: organization)
+      user_ids.each do |user_id|
+        room.participants.new(user_id: user_id)
+      end
+      room.name = User.find(user_ids.last).first_name
+      room.save
+      room
     end
-    room.name = User.find(user_ids.last).first_name
-    room.save
-    room
   end
 
   def recipient(sender)
