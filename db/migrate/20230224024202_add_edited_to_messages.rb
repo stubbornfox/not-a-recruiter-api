8eda47df24df12be3fcb8ddffa4b40bcdd15447c
Ha Le <nhiha001@gmail.com>
2023-02-24 15:59:27 +0700

    [f] change column type of deleted

--- db/migrate/20230224024202_add_edited_to_messages.rb
+++ db/migrate/20230224024202_add_edited_to_messages.rb
@@ -1,6 +1,6 @@
 class AddEditedToMessages < ActiveRecord::Migration[7.0]
   def change
     add_column :messages, :edited, :datetime
-    add_column :messages, :deleted, :datetime
+    change_column :messages, :deleted, :datetime
   end
 end
