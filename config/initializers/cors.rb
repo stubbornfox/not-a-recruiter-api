40442f7aad9cd8e421b5a7432970aaef8d8a087a
Ha Le <nhiha001@gmail.com>
2023-08-20 14:15:23 +0200

    [f] Change the config of cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins Rails.application.credentials[Rails.env.to_sym][:allowed_origins]
+
 
     resource '*',
              headers: :any,
